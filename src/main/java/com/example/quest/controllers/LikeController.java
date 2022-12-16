57fdcbc06dc2acb80f4ce0eada6dad0c2ebe9c6a
Görkem <gorkem_yelken@hotmail.com>
2022-12-16 21:08:05 +0300

    LikeService ve LikeController oluşturuldu.

---
+++ src/main/java/com/example/quest/controllers/LikeController.java
@@ -0,0 +1,40 @@
+package com.example.quest.controllers;
+
+import com.example.quest.entities.Like;
+import com.example.quest.requests.LikeCreateRequest;
+import com.example.quest.services.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/likes")
+public class LikeController {
+    private LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @GetMapping
+    public List<Like> getAllLikes(@RequestParam Optional<Long> userId,
+                                  @RequestParam Optional<Long> postId) {
+        return likeService.getAllLikesWithParam(userId, postId);
+    }
+
+    @PostMapping
+    public Like createOneLike(@RequestBody LikeCreateRequest request) {
+        return likeService.createOneLike(request);
+    }
+
+    @GetMapping("/{likeId}")
+    public Like getOneLike(@PathVariable Long likeId) {
+        return likeService.getOneLikeById(likeId);
+    }
+
+    @DeleteMapping("/{likeId}")
+    public void deleteOneLike(@PathVariable Long likeId) {
+        likeService.deleteOneLikeById(likeId);
+    }
+}
--- src/main/java/com/example/quest/services/LikeService.java
+++ src/main/java/com/example/quest/services/LikeService.java
@@ -1,14 +1,58 @@
 package com.example.quest.services;
 
+import com.example.quest.entities.Like;
+import com.example.quest.entities.Post;
+import com.example.quest.entities.User;
 import com.example.quest.repositories.LikeRepository;
+import com.example.quest.requests.LikeCreateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LikeService {
     private LikeRepository likeRepository;
-    @Autowired
-    public LikeService(LikeRepository likeRepository) {
+    private UserService userService;
+    private PostService postService;
+
+    public LikeService(LikeRepository likeRepository, UserService userService,
+                       PostService postService) {
         this.likeRepository = likeRepository;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    public List<Like> getAllLikesWithParam(Optional<Long> userId, Optional<Long> postId) {
+        if(userId.isPresent() && postId.isPresent()) {
+            return likeRepository.findByUserIdAndPostId(userId.get(), postId.get());
+        }else if(userId.isPresent()) {
+            return likeRepository.findByUserId(userId.get());
+        }else if(postId.isPresent()) {
+            return likeRepository.findByPostId(postId.get());
+        }else
+            return likeRepository.findAll();
+    }
+
+    public Like getOneLikeById(Long LikeId) {
+        return likeRepository.findById(LikeId).orElse(null);
+    }
+
+    public Like createOneLike(LikeCreateRequest request) {
+        User user = userService.getOneUser(request.getUserId());
+        Post post = postService.getOnePostById(request.getPostId());
+        if(user != null && post != null) {
+            Like likeToSave = new Like();
+            likeToSave.setId(request.getId());
+            likeToSave.setPost(post);
+            likeToSave.setUser(user);
+            return likeRepository.save(likeToSave);
+        }else
+            return null;
+    }
+
+    public void deleteOneLikeById(Long likeId) {
+        likeRepository.deleteById(likeId);
     }
 }
